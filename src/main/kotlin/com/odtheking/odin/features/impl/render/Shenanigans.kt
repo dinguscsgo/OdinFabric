a853793432f6957e9d62dd4ce242cc62018fd3d7
Dinugs <heckoff7770@gmail.com>
2026-04-01 15:47:04 -0400

    Update Shenanigans.kt

--- src/main/kotlin/com/odtheking/odin/features/impl/render/Shenanigans.kt
+++ src/main/kotlin/com/odtheking/odin/features/impl/render/Shenanigans.kt
@@ -21,17 +21,7 @@ object Shenanigans {
     )
 
     private val logoList = listOf(
-        Logos(Identifier.fromNamespaceAndPath("odin", "textures/logo.png"), Vec3(73.5, 224.5, 11.0), 6f, 3f, 0f),
-        Logos(Identifier.fromNamespaceAndPath("odin", "textures/logo.png"), Vec3(73.5, 216.5, 50.0), 10f, 7f, 180f),
-        Logos(Identifier.fromNamespaceAndPath("odin", "textures/logo.png"), Vec3(73.5, 216.5, 32.0), 10f, 7f, 0f),
-        Logos(Identifier.fromNamespaceAndPath("odin", "textures/logo.png"), Vec3(100.5, 127.5, 143.0), 8f, 5f, 180f),
-        Logos(Identifier.fromNamespaceAndPath("odin", "textures/logo.png"), Vec3(111.0, 127.5, 132.5), 8f, 5f, 90f),
-        Logos(Identifier.fromNamespaceAndPath("odin", "textures/logo.png"), Vec3(8.5, 127.5, 143.0), 8f, 5f, 180f),
-        Logos(Identifier.fromNamespaceAndPath("odin", "textures/logo.png"), Vec3(-2.0, 127.5, 132.5), 8f, 5f, 270f),
-        Logos(Identifier.fromNamespaceAndPath("odin", "textures/logo.png"), Vec3(-2.0, 127.5, 40.5), 8f, 5f, 270f),
-        Logos(Identifier.fromNamespaceAndPath("odin", "textures/logo.png"), Vec3(8.5, 127.5, 30.0), 8f, 5f, 0f),
-        Logos(Identifier.fromNamespaceAndPath("odin", "textures/logo.png"), Vec3(54.5, 116.5, 118.0), 9f, 6f, 180f),
-        Logos(Identifier.fromNamespaceAndPath("odin", "textures/logo.png"), Vec3(54.5, 64.0, 84.0), 3f, 2f, 0f),
+
         Logos(Identifier.fromNamespaceAndPath("odin", "textures/job.png"), Vec3(54.5, 23.5, 40.0), 25f, 35f, 0f)
     )
 
